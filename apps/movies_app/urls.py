e9e49aad5a5f14b22203b3b775d3052197620161
Mark Ayers <markthema3@gmail.com>
2016-09-19 10:48:16 -0700

    Created models, views, and templates

--- apps/movies_app/urls.py
+++ apps/movies_app/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index),
-	url(r'^users$',views.show),
+    url(r'^$', views.index),
+    url(r'^addmovie$', views.addmovie),
+    url(r'^addactor$', views.addactor),
+	url(r'^movies/(?P<id>\d+)$', views.movie),
+    url(r'^actors/(?P<id>\d+)$', views.actor)
 ]
